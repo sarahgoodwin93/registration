2e9ba4f5693aa23d1536f70d6d4ef3e8692d6dda
Sarah <sarah@slg-coding.com>
2025-01-28 15:55:30 +0000

    add event and submission models

--- base/models.py
+++ base/models.py
@@ -12,5 +12,22 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
     
 
+class Event(models.Model):
+    name = models.CharField(max_length=1000)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, blank=True)
+    date = models.DateTimeField()
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
 
 
+class Submission(models.Model):
+    participant = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
+    details = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return str(self.event) + ' --- ' + str(self.participant)
