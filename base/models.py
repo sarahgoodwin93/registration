6b291724b575cecf2ce3899cf5ff4fe3adfe964d
Sarah <sarah@slg-coding.com>
2025-01-28 15:37:19 +0000

    set up project and first models

---
+++ base/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=100, null=True)
+    email = models.EmailField(unique=True, max_length=254, null=True)
+    bio = models.TextField(null=True, blank=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+
+
+
