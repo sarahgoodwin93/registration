018a37c63655cf88acdf28d045b700a48940e5ae
Sarah <sarah@slg-coding.com>
2025-01-29 12:49:18 +0000

    add event page

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name="home"),
+    path('event/<str:pk>/', views.event_page, name="event"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import User, Event, Submission
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 
@@ -8,3 +10,10 @@ def home_page(request):
     events = Event.objects.all()
     context = {'users':users, 'events':events}
     return render(request, 'home.html', context)
+
+
+def event_page(request, pk):
+    event = get_object_or_404(Event, id=pk)
+    context = {'event':event}
+    return render(request, 'event.html', context)
+
